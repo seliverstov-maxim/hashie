f38542d6e675194774b6ef34a80ff021b98e84c6
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-06-04 17:49:28 +0400

    Refactor Mash class.

--- lib/hashie/mash.rb
+++ lib/hashie/mash.rb
@@ -12,18 +12,18 @@ module Hashie
     def method_missing(m, *args, &block)
       operator_name = m[-1].to_s
       case operator_name
-      when '?' then
-         var_name = operator_name.scan(/^(.*)\?/).first[0]
+      when "?" then
+         var_name = m.to_s.scan(/^(.*)\?/).first[0]
          self.define_singleton_method(m) do
-           self.key? var_name
+            self.key? var_name
          end
          self.send(m)
       when "=" then
-        var_name = m.to_s.scan(/^(.*)\=/).first[0]
-        self[var_name] = args[0]
+        var_name = m.to_s.scan(/^(.*)\s*\=/).first[0]
         self.define_singleton_method(m) do |v|
           self[var_name] = v
         end
+        self.send(m, args[0])
       else
         self.define_singleton_method(m) do
           self[m.to_s]
