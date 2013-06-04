28b8f89abaf93f00ebc6f630279ec8980fbfb7ee
YuryKorotin <dreadnoughtfull@yandex.ru>
2013-06-04 14:46:18 +0400

    Adding tests for name

--- lib/hashie.rb
+++ lib/hashie.rb
@@ -2,7 +2,23 @@ require "hashie/version"
 
 module Hashie
   class Mash
-    def initialize
+    attr_accessor :name
+    def initialize(name = nil)
+      @name = name
+    end
+
+    def name_nil?
+      @name.nil?
+    end
+
+    def name?
+      !name_nil?
+    end
+    def author
+      self.class
+    end
+
+    def method_missing(m, *args, &block)
 
     end
   end
--- test/lib/mash_test.rb
+++ test/lib/mash_test.rb
@@ -5,7 +5,24 @@ class MashTest < TestCase
     @mash = Hashie::Mash.new
   end
 
-  def test_deep_merge
-    assert true
+  def test_mash_should_have_no_name
+    mash = Hashie::Mash.new
+    assert_equal mash.name?, false
+    assert_equal mash.name, nil
   end
+
+  def test_mash_should_have_name
+    mash = Hashie::Mash.new("test")
+    assert mash.name?
+    assert_equal mash.name, "test"
+  end
+
+  def test_should_inspect
+    @mash.inspect
+  end
+
+  def test_should_get_author
+    assert @mash.author, @mash.class
+  end
+
 end
