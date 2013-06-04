8a3022bc98d3fa3ef6f815a2c4950f344450eafc
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-06-04 20:54:52 +0400

    Implement Dash.

--- lib/hashie/dash.rb
+++ lib/hashie/dash.rb
@@ -1,29 +1,53 @@
 require "hashie/version"
 module Hashie
   class Dash
+    def self.get_default
+      @default
+    end
+
+    def self.get_required
+      @required
+    end
+
     def initialize(options)
-      @options = options
+      @values = {}
+      @required = self.class.get_required
+      @default = self.class.get_default
+
+      #@values.merge!(@required)
+      @values.merge!(@default)
+      @values.merge!(options)
+
+      if (@required.to_a - @values.to_a).empty?
+        raise ArgumentError
+      end
     end
     def self.property(name, options = {})
+      @default ||= {}
+      @required ||= {}
+
+      @default[name] = options[:default]
+      @required[name] = options[:required]
+
       define_method "#{name}=" do |v|
-        if @options[:required] && v.nil?
+        if @required[name] && v.nil?
           raise ArgumentError
         end
-        @options[name] = v
+        @values[name] = v
       end
       define_method "[]=" do |k, v|
-        if @options[:required] && v.nil?
+        if @required[name] && v.nil?
           raise ArgumentError
         end
-        @options[k] = v
+        @values[k] = v
       end
 
 
       define_method name do
-        @options[name]
+        @values[name]
       end
       define_method "[]" do |k|
-        @options[k]
+        @values[k]
       end
     end
   end
