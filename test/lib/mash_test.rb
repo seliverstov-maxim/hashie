8a933854aba141161f6e538486e6ff3bfd1df8fb
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-06-04 17:50:51 +0400

    Refactor test.

--- test/lib/mash_test.rb
+++ test/lib/mash_test.rb
@@ -5,13 +5,17 @@ class MashTest < TestCase
     @mash = Hashie::Mash.new
   end
 
-  def test_mash_should_have_no_name
-    mash = Hashie::Mash.new
-    assert !mash.name?
-    assert_nil mash.name
-    mash.name= "test"
-    assert_equal "test", mash.name
-    raise mash.inspect
-    assert mash.name?
+  def test_mash_should_return_nil
+    assert_nil @mash.name
+  end
+
+  def test_mash_should_return_false
+    assert !@mash.name?
+  end
+
+  def test_mash_setter
+    @mash.name = "test"
+    assert_equal "test", @mash.name
+    assert @mash.name?
   end
 end
