3ba17d69ac3f1816f32d748db447421fe4c9b12c
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-06-04 19:29:50 +0400

    Implement test_dash_should_set_property test.

--- test/lib/dash_test.rb
+++ test/lib/dash_test.rb
@@ -11,6 +11,13 @@ class DashTest < TestCase
   end
 
   def test_dash_should_set_property
+    @dash.email = "example@mail.ru"
+    assert_equal "example@mail.ru", @dash.email
+    assert_equal "example@mail.ru", @dash[:email]
+
+    @dash[:name] = "example name"
+    assert_equal "example name", @dash.name
+    assert_equal "example name", @dash[:name]
   end
 
   def test_dash_should_return_property
