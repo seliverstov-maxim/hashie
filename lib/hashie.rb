007033da70e32f6835bf09c7fab2ad04ee8ef5d6
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-06-04 15:28:37 +0400

    Modify hashie.rb. Add BaseHasie class.

--- lib/hashie.rb
+++ lib/hashie.rb
@@ -1,25 +1,8 @@
 require "hashie/version"
-
 module Hashie
-  class Mash
-    attr_accessor :name
-    def initialize(name = nil)
-      @name = name
-    end
+  class BaseHash < Hash
 
-    def name_nil?
-      @name.nil?
-    end
-
-    def name?
-      !name_nil?
-    end
-    def author
-      self.class
-    end
-
-    def method_missing(m, *args, &block)
-
-    end
   end
+
+  autoload :Mash, 'hashie/mash'
 end
